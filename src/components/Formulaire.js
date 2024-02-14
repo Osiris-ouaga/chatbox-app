f681a242b22b25346187f013eaee0b291b7b6cca
Prince Divin MACKPAYEN <54855716+Osiris-ouaga@users.noreply.github.com>
2024-02-14 12:59:49 +0100

    Components Message added

--- src/components/Formulaire.js
+++ src/components/Formulaire.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 
 class Formulaire extends Component {
+    hundleSubmit = event =>{
+        console.log('Submit')
+    }
     render (){
         return(
             <form
-                className='form'>
+                className='form'
+                 onSubmit={this.hundleSubmit}>
                     <textarea
                         required
                         maxLength={140}/>
