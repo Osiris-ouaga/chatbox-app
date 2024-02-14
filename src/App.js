a5fa1b6da8418b86cd8da5b83e271288be93d649
Prince Divin MACKPAYEN <54855716+Osiris-ouaga@users.noreply.github.com>
2024-02-14 12:22:08 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='box'>
+        <div>
+          <div className='messages'>
+            Message
+          </div>
+        </div>
+        <Formulaire />
+      </div>
+    )
+  }
+}
+
+export default App
